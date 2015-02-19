e3223ae2bb76f106ad0bbf6bd1956e353dc077eb
Philipp Tsipman <philtsip@yahoo.com>
2015-02-18 23:59:10 -0500

    rewrite date math and array math

--- array.math.js
+++ array.math.js
@@ -1,3 +1,7 @@
+//  JAVASCRIPT ARRAY MATH UTILS
+//  Copyright (c) 2015 Philipp Tsipman 
+//  v 1.0 on 2015-02-04
+
 /*  JAVASCRIPT ARRAY MATH UTILS
     Use these together for forecasting functions  
 
@@ -12,7 +16,7 @@
     aicc
     
 	Math.min.apply
-	Math.min.apply
+	Math.max.apply
 
     numericLength
     sumArray
@@ -22,6 +26,7 @@
     logArray
     expArray
     diffArray
+	percentdiffArray
     roundArray
     
     Array.prototype.transpose
@@ -29,255 +34,336 @@
     pastorFuture
 */
 
-//  ARRAY MATH
+(function(){
 
-// Source: http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
-function isNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-//  round(number, decimals), e.g. round(10.3333, 2)
-//  Source: http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript#comment42209497_19722641
-function round(number, decimals) {
-  return +(Math.round(number + "e+" + decimals)  + "e-" + decimals);
-}
-
-//  Math.log(number, base), e.g. Math.log(10, 2)
-//  Source: http://stackoverflow.com/questions/3019278/how-can-i-specify-the-base-for-math-log-in-javascript
-Math.log = (function() {
-    var log = Math.log;
-    return function(n, base) {
-        return log(n)/(base ? log(base) : 1);
-    };
-})();
-
-// AICC
-// aicc(n_after_diff, p, q, ss, include_constant)
-// Example: aicc(786, 2, 1, 1.05297, false) == -5191.607634684157
-
-function aicc (n, p, q, ss, constant) {
-	var const_factor = 0;
-	if (constant)
-		const_factor = 1;
-
-	return (n*Math.log(ss/n))+(2*(p+q+(1+const_factor))*n/(n-p-q-(2+const_factor)));
-}
-
-// Min of array
-Math.min.apply(null, arr),
-
-// Max of array
-Math.max.apply(null, arr);
-	
-function numericLength(array) {
-	        
-    var len = 0;
-    if (array instanceof Array) {
-    	for (var i = 0; i < array.length; i++) {
-    		if (isNumber(array[i]))
-    		    len++;
-    	}
-    	return len;
-    }
-    else
-    	return null;
+    'use strict';
     
-};
+	//  ARRAY MATH
 
-function sumArray(array) {
-
-    var i = array.length,
-        sum = 0;
-    while (i--) {
-        if (isNumber(array[i]))
-            sum += array[i];
-    }
-
-    return sum;
-};
-
-function meanArray(array) {
-    // requires sumArray
-    // requires numericLength
-    return sumArray(array) / numericLength(array);
-};
-
-
-// sumSquares(array, false)
-
-function sumSquares(input_array, constant) {
-    // requires meanArray, sumArray
-    
-    var mean_array = 0;
-    if (constant)
-        mean_array = meanArray(input_array);
-
-    var sum = 0;
-    if (input_array instanceof Array) {
-    	for (var i = 0; i < input_array.length; i++) {
-            if (isNumber(input_array[i]))
-    		    sum += Math.pow((input_array[i]-mean_array), 2);
-    	}
-    	return sum;
-    }
-    else
-    	return null;
-};
-
-
-
-// logArray([array], optional_base)
-// Assumes natural log if base is omitted. Returns an array of logarithms 
-// Example: logArray([5, 2], 10); logArray([20, 7, Math.E]);
-
-function logArray(input_array, base) {
-    
-	var output_array = [];
-	if (input_array instanceof Array) {
-		for (var i = 0; i < input_array.length; i++) {
-			output_array.push(Math.log(input_array[i], base));
-		}
-		return output_array;
+	// Source: http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric
+	function isNumber(n) {
+	  return !isNaN(parseFloat(n)) && isFinite(n);
 	}
-	else
-		return null;
-};
 
-// expArray(array), e.g. expArray([5, 2]);
+	//  round(number, decimals), e.g. round(10.3333, 2)
+	//  Source: http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript#comment42209497_19722641
+	function round(number, decimals) {
+	  return +(Math.round(number + "e+" + decimals)  + "e-" + decimals);
+	}
 
-function expArray(input_array) {
+	//  Math.log(number, base), e.g. Math.log(10, 2)
+	//  Source: http://stackoverflow.com/questions/3019278/how-can-i-specify-the-base-for-math-log-in-javascript
+	Math.log = (function() {
+	    var log = Math.log;
+	    return function(n, base) {
+	        return log(n)/(base ? log(base) : 1);
+	    };
+	})();
+
+	// AICC
+	// aicc(n_after_diff, p, q, ss, include_constant)
+	// Example: aicc(786, 2, 1, 1.05297, false) == -5191.607634684157
+
+	function aicc (n, p, q, ss, constant) {
+		var const_factor = 0;
+		if (constant)
+			const_factor = 1;
+
+		return (n*Math.log(ss/n))+(2*(p+q+(1+const_factor))*n/(n-p-q-(2+const_factor)));
+	}
+
+	// Min of array
+	// Math.min.apply(null, arr);
+
+	// Max of array
+	// Math.max.apply(null, arr);
+	
+	function numericLength(array) {
+	        
+	    var len = 0;
+	    if (array instanceof Array) {
+	    	for (var i = 0; i < array.length; i++) {
+	    		if (isNumber(array[i]))
+	    		    len++;
+	    	}
+	    	return len;
+	    }
+	    else
+	    	return null;
     
-    var output_array = [];
-    if (input_array instanceof Array) {
-    	for (var i = 0; i < input_array.length; i++) {
-    		output_array.push(Math.exp(input_array[i]));
-    	}
-    	return output_array;
-    }
-    else
-    	return null;
-};
+	};
+
+	function sumArray(array) {
+
+	    var i = array.length,
+	        sum = 0;
+	    while (i--) {
+	        if (isNumber(array[i]))
+	            sum += array[i];
+	    }
+
+	    return sum;
+	};
+
+	function meanArray(array) {
+	    // requires sumArray
+	    // requires numericLength
+	    return sumArray(array) / numericLength(array);
+	};
 
 
-// diffArray([array])
-// Returns an array of differences between array elements.  First element returned is NaN, null evaluates to 0
-// Example: diffArray([5, 2]);
+	// sumSquares(array, false)
 
-function diffArray(input_array) {
+	function sumSquares(input_array, constant) {
+	    // requires meanArray, sumArray
     
-    var output_array = [];
-    if (input_array instanceof Array) {
-    	for (var i = 0; i < input_array.length; i++) {
-    		output_array.push(input_array[i] - input_array[i-1]);
-    	}
-    	return output_array;
-    }
-    else
-    	return null;
-};
+	    var mean_array = 0;
+	    if (constant)
+	        mean_array = meanArray(input_array);
 
-// roundArray([array], optional_decimal_places)
-// Assumes 0 decimal places as the default
-// Example: roundArray([5.2222, 2.5555], 3);
+	    var sum = 0;
+	    if (input_array instanceof Array) {
+	    	for (var i = 0; i < input_array.length; i++) {
+	            if (isNumber(input_array[i]))
+	    		    sum += Math.pow((input_array[i]-mean_array), 2);
+	    	}
+	    	return sum;
+	    }
+	    else
+	    	return null;
+	};
 
-// Will not work for ([1.005],2) which comes out as [1] instead of [1.01], 
-// see: http://stackoverflow.com/questions/11832914/round-up-to-2-decimal-places-in-javascript#comment26122771_11832950
 
-function roundArray(input_array, decimals) {
+
+	// logArray([array], optional_base)
+	// Assumes natural log if base is omitted. Returns an array of logarithms 
+	// Example: logArray([5, 2], 10); logArray([20, 7, Math.E]);
+
+	function logArray(input_array, base) {
     
-    var output_array = [];
-    if (!decimals)
-        decimals = 0;
+		var output_array = [];
+		if (input_array instanceof Array) {
+			for (var i = 0; i < input_array.length; i++) {
+				output_array.push(Math.log(input_array[i], base));
+			}
+			return output_array;
+		}
+		else
+			return null;
+	};
+
+	// expArray(array), e.g. expArray([5, 2]);
+
+	function expArray(input_array) {
+    
+	    var output_array = [];
+	    if (input_array instanceof Array) {
+	    	for (var i = 0; i < input_array.length; i++) {
+	    		output_array.push(Math.exp(input_array[i]));
+	    	}
+	    	return output_array;
+	    }
+	    else
+	    	return null;
+	};
+
+
+	// diffArray([array], optional_span)
+	// Returns an array of differences between array elements.  Initial elements returned as null.  2000 - 2010 = 10 year span.
+	// Example: diffArray([5, 2]);
+
+	function diffArray(input_array, span) {
+    
+	    if (input_array instanceof Array) {
+
+		    var output_array = [];
+		
+			if (!span)
+				span = 1;
+		
+			for (var i = 0; i < span; i++) 
+				output_array.push(null);
+	
+			for (var i = span; i < input_array.length; i++)
+				output_array.push(input_array[i] - input_array[i-span]);
+
+	    	return output_array;
+	    }
+	    else
+	    	return null;
+	};
+
+	// percentdiffArray([array], optional_span)
+	// Returns an array of % change between array elements.  Initial elements returned as null
+	// Example: percentdiffArray([5, 2]);
+
+	function percentdiffArray(input_array, span) {
+    
+	    if (input_array instanceof Array) {
+
+		    var output_array = [];
+		
+			if (!span)
+				span = 1;
+		
+			for (var i = 0; i < span; i++) 
+				output_array.push(null);
+	
+			for (var i = span; i < input_array.length; i++) {
+				if (input_array[i-span] == 0)
+					output_array.push(null);
+				else 
+					output_array.push(input_array[i] / input_array[i-span] -1)
+			}
+
+	    	return output_array;
+	    }
+	    else
+	    	return null;
+	};
+
+	// roundArray([array], optional_decimal_places)
+	// Assumes 0 decimal places as the default
+	// Example: roundArray([5.2222, 2.5555], 3);
+
+	// Will not work for ([1.005],2) which comes out as [1] instead of [1.01], 
+	// see: http://stackoverflow.com/questions/11832914/round-up-to-2-decimal-places-in-javascript#comment26122771_11832950
+
+	function roundArray(input_array, decimals) {
+    
+	    var output_array = [];
+	    if (!decimals)
+	        decimals = 0;
         
-    if (input_array instanceof Array) {
-    	for (var i = 0; i < input_array.length; i++) {
-    		output_array.push(round(input_array[i], decimals));
-    	}
-    	return output_array;
+	    if (input_array instanceof Array) {
+	    	for (var i = 0; i < input_array.length; i++) {
+	    		output_array.push(round(input_array[i], decimals));
+	    	}
+	    	return output_array;
+	    }
+	    else
+	    	return null;
+	};
+
+	//  ARRAY TRANSFORMATIONS
+
+	// Clone array: http://stackoverflow.com/questions/3978492/javascript-fastest-way-to-duplicate-an-array-slice-vs-for-loop
+	// var b = a.slice(); 
+	// var outputArray = inputArray.slice();
+
+
+	// Source: http://stackoverflow.com/questions/4492678/to-swap-rows-with-columns-of-matrix-in-javascript-or-jquery
+	Array.prototype.transpose = function() {
+
+	  // Calculate the width and height of the Array
+	  var a = this,
+	    w = a.length ? a.length : 0,
+	    h = a[0] instanceof Array ? a[0].length : 0;
+
+	  // In case it is a zero matrix, no transpose routine needed.
+	  if(h === 0 || w === 0) { return []; }
+
+	  /**
+	   * @var {Number} i Counter
+	   * @var {Number} j Counter
+	   * @var {Array} t Transposed data is stored in this array.
+	   */
+	  var i, j, t = [];
+
+	  // Loop through every item in the outer array (height)
+	  for(i=0; i<h; i++) {
+
+	    // Insert a new row (array)
+	    t[i] = [];
+
+	    // Loop through every item per item in outer array (width)
+	    for(j=0; j<w; j++) {
+
+	      // Save transposed data.
+	      t[i][j] = a[j][i];
+	    }
+	  }
+
+	  return t;
+	};
+
+	// assume both arrays are sorted by first column
+	// second array must have exactly 2 columns
+	// first array must include all years
+	// a = [[2007,2], [2008, 4]];
+	// b = [[2008, 18],[2009, 4], [2010, 8]];
+
+	function addArray (output_array, second_array) {
+	    if (second_array.length > 0 && second_array[0].length == 2) {        
+
+	        // iterate through each row of output_array
+	        for (var i = 0; i < output_array.length; i++) {
+	            var j = 0;
+	            while ((j < second_array.length) && (output_array[i][0] > second_array[j][0]))
+	            	j++;
+	            if ((j < second_array.length) && (output_array[i][0] == second_array[j][0]))
+	                output_array[i].push(second_array[j][1]);
+	            else
+	                output_array[i].push(null);  
+	        }
+	    }
+	    return output_array;
+	}
+
+	// assume array is sorted by first column
+	// assume array has exactly 2 columns
+	// a = [[2008, 18],[2009, 4], [2010, 8], [2012, 8], [2013, 8], [2014, 8]];
+
+	function pastorFuture(array) {
+	    var array_length = array.length;
+	    // if (array_length)
+	    //     var array_width = array[0].length;
+	    var year = new Date().getFullYear();
+	    if (array_length && array[0][0] <= year && array[array_length-1][0] > year) {
+	        for (var i = 0; i < array_length; i++) {
+	                if (array[i][0] < year)
+	                    array[i].push(null);
+	                else if (array[i][0] == year)
+	                    array[i].push(array[i][1]);
+	                else
+	                    array[i] = [array[i][0],null,array[i][1]];                
+	        }
+	    }
+	    return array;
+	}
+
+    // Exports and modularity
+	var a = {};
+	a.isNumber = isNumber;
+    a.round = round,
+    a.aicc = aicc,
+
+    a.numericLength = numericLength,
+    a.sumArray = sumArray,
+    a.meanArray = meanArray,
+    a.sumSquares = sumSquares,
+
+    a.logArray = logArray,
+    a.expArray = expArray,
+    a.diffArray = diffArray,
+	a.percentdiffArray = percentdiffArray,
+    a.roundArray = roundArray,
+
+    a.addArray = addArray,
+    a.pastorFuture = pastorFuture;
+	
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = a;
     }
-    else
-    	return null;
-};
 
-//  ARRAY TRANSFORMATIONS
-
-
-// Source: http://stackoverflow.com/questions/4492678/to-swap-rows-with-columns-of-matrix-in-javascript-or-jquery
-Array.prototype.transpose = function() {
-
-  // Calculate the width and height of the Array
-  var a = this,
-    w = a.length ? a.length : 0,
-    h = a[0] instanceof Array ? a[0].length : 0;
-
-  // In case it is a zero matrix, no transpose routine needed.
-  if(h === 0 || w === 0) { return []; }
-
-  /**
-   * @var {Number} i Counter
-   * @var {Number} j Counter
-   * @var {Array} t Transposed data is stored in this array.
-   */
-  var i, j, t = [];
-
-  // Loop through every item in the outer array (height)
-  for(i=0; i<h; i++) {
-
-    // Insert a new row (array)
-    t[i] = [];
-
-    // Loop through every item per item in outer array (width)
-    for(j=0; j<w; j++) {
-
-      // Save transposed data.
-      t[i][j] = a[j][i];
+    if (typeof ender === 'undefined') {
+        window.am = a;
     }
-  }
 
-  return t;
-};
-
-// assume both arrays are sorted by first column
-// second array must have exactly 2 columns
-// first array must include all years
-// a = [[2007,2], [2008, 4]];
-// b = [[2008, 18],[2009, 4], [2010, 8]];
-
-function addArray (output_array, second_array) {
-    if (second_array.length > 0 && second_array[0].length == 2) {        
-
-        // iterate through each row of output_array
-        for (var i = 0; i < output_array.length; i++) {
-            var j = 0;
-            while ((j < second_array.length) && (output_array[i][0] > second_array[j][0]))
-            	j++;
-            if ((j < second_array.length) && (output_array[i][0] == second_array[j][0]))
-                output_array[i].push(second_array[j][1]);
-            else
-                output_array[i].push(null);  
-        }
+    if (typeof define === "function" && define.amd) {
+        define('am', [], function () { 
+            return a;
+        });
     }
-    return output_array;
-}
 
-// assume array is sorted by first column
-// assume array has exactly 2 columns
-// a = [[2008, 18],[2009, 4], [2010, 8], [2012, 8], [2013, 8], [2014, 8]];
-
-function pastorFuture(array) {
-    var array_length = array.length;
-    // if (array_length)
-    //     var array_width = array[0].length;
-    var year = new Date().getFullYear();
-    if (array_length && array[0][0] <= year && array[array_length-1][0] > year) {
-        for (var i = 0; i < array_length; i++) {
-                if (array[i][0] < year)
-                    array[i].push(null);
-                else if (array[i][0] == year)
-                    array[i].push(array[i][1]);
-                else
-                    array[i] = [array[i][0],null,array[i][1]];                
-        }
-    }
-    return array;
-}
+}).call(this);
--- date.math.js
+++ date.math.js
@@ -1,107 +1,144 @@
-// DATA STRUCTURE
-//
-// var series = {
-// 	"meta": {
-// 		"columns": ["date", "volume", "adj_close"],
-// 	},
-// 	"data": [
-// 		[],
-// 		[],
-// 		[]
-// 	]
-// }
+//  JAVASCRIPT DATE MATH UTILS
+//  Copyright (c) 2015 Philipp Tsipman 
+//  v 1.0 on 2015-02-04
 
-// clone array: http://stackoverflow.com/questions/3978492/javascript-fastest-way-to-duplicate-an-array-slice-vs-for-loop
-// var b = a.slice(); 
-// var outputArray = inputArray.slice();
+/*  JAVASCRIPT DATE MATH UTILS
 
-// var series = {"data":[["2014/03/27","2014/04/28","2014/04/30"],[13100,41100,10800],[558.46,559.99,556.97]]};
+	getWeek
+	convertSeriesFrequency
+*/
 
-var series = require('./test/goog_daily.json');
 
-// getWeek: https://gist.github.com/dblock/1081513
-Date.prototype.getWeek = function() { 
+(function(){
+
+    'use strict';
+    
+	// getWeek: https://gist.github.com/dblock/1081513
+	Date.prototype.getWeek = function() { 
 	
-  var d = this;
+	  var d = this;
  
-  // Create a copy of this date object  
-  var target  = new Date(d.valueOf());  
+	  // Create a copy of this date object  
+	  var target  = new Date(d.valueOf());  
   
-  // ISO week date weeks start on monday  
-  // so correct the day number  
-  var dayNr   = (d.getDay() + 6) % 7;  
+	  // ISO week date weeks start on monday  
+	  // so correct the day number  
+	  var dayNr   = (d.getDay() + 6) % 7;  
  
-  // Set the target to the thursday of this week so the  
-  // target date is in the right year  
-  target.setDate(target.getDate() - dayNr + 3);  
+	  // Set the target to the thursday of this week so the  
+	  // target date is in the right year  
+	  target.setDate(target.getDate() - dayNr + 3);  
  
-  // ISO 8601 states that week 1 is the week  
-  // with january 4th in it  
-  var jan4    = new Date(target.getFullYear(), 0, 4);  
+	  // ISO 8601 states that week 1 is the week  
+	  // with january 4th in it  
+	  var jan4    = new Date(target.getFullYear(), 0, 4);  
  
-  // Number of days between target date and january 4th  
-  var dayDiff = (target - jan4) / 86400000;    
+	  // Number of days between target date and january 4th  
+	  var dayDiff = (target - jan4) / 86400000;    
  
-  // Calculate week number: Week 1 (january 4th) plus the    
-  // number of weeks between target date and january 4th    
-  var weekNr = 1 + Math.ceil(dayDiff / 7);    
+	  // Calculate week number: Week 1 (january 4th) plus the    
+	  // number of weeks between target date and january 4th    
+	  var weekNr = 1 + Math.ceil(dayDiff / 7);    
  
-  return weekNr;    
+	  return weekNr;    
  
-}
-
-// Assumes first column is standard-formatted dates (eg. YYYY/MM/DD)
-function convertSeriesFrequency (inputArray, frequency) {
-	var numCols = inputArray.data.length;
-	var numRows = inputArray.data[0].length;
-	var outputArray = [], current = 0, next = 0;
-	
-	outputArray.meta = inputArray.meta; 
-	outputArray.data = []; for (var i = 0; i < numCols; i++) outputArray.data.push([]);
-	
-	if (true) { // TODO: check for date format
-		if ( (new Date(inputArray.data[0][0])) < (new Date(inputArray.data[0][1])) ) {// chronological 
-
-			// iterate through dates
-			current = new Date(inputArray.data[0][0]);
-			
-			for (var j = 1; j < numRows; j++) {
-				next = new Date(inputArray.data[0][j]);
-						
-				if ( frequency == "weekly" && ( current.getFullYear() == next.getFullYear() ) && ( current.getWeek() == next.getWeek() ) ) {
-					current = next;	
-				}
-				
-				else if ( frequency == "monthly" && ( current.getFullYear() == next.getFullYear() ) && ( (current.getMonth()+1) == (next.getMonth()+1) ) ) { // note: month starts with 0
-					current = next;	
-				}
-								
-				else if ( frequency == "annual" && ( current.getFullYear() == next.getFullYear() ) ) {
-					current = next;	
-				}
-
-				else {
-					for (var k = 0; k < numCols; k++) {
-						outputArray.data[k].push(inputArray.data[k][j-1]);
-					}
-					current = next;					
-				}
-			}
-			
-			// always push last row
-			for (var k = 0; k < numCols; k++) {
-				outputArray.data[k].push(inputArray.data[k][numRows-1]);
-			}
-		
-		}		
-		else
-			return null; // TODO: reverse chronological?
 	}
-	else
-		return null; // bad format
-			
-	return outputArray;
-}
 
-// Options: weekly, monthly, annual
-console.log(convertSeriesFrequency(series, "weekly"));
+	// Options: weekly, monthly, annual
+	// Assumes first data column is standard-formatted dates (eg. YYYY/MM/DD)
+
+	// DATA STRUCTURE
+	//
+	// var series = {
+	// 	"meta": {
+	// 		"columns": ["date", "volume", "adj_close"],
+	// 	},
+	// 	"data": [
+	// 		[],
+	// 		[],
+	// 		[]
+	// 	]
+	// }
+
+	function convertSeriesFrequency (inputObj, frequency) {
+		var numCols = inputObj.data.length;
+		var numRows = inputObj.data[0].length;
+		var outputObj = {}, current = 0, next = 0;
+	
+		outputObj.meta = inputObj.meta; 
+		outputObj.data = []; for (var i = 0; i < numCols; i++) outputObj.data.push([]);
+	
+		if (true) { // TODO: check for date format
+			if ( (new Date(inputObj.data[0][0])) < (new Date(inputObj.data[0][1])) ) {// chronological 
+
+				// iterate through dates
+				current = new Date(inputObj.data[0][0]);
+			
+				for (var j = 1; j < numRows; j++) {
+					next = new Date(inputObj.data[0][j]);
+						
+					if ( frequency == "weekly" && ( current.getFullYear() == next.getFullYear() ) && ( current.getWeek() == next.getWeek() ) ) {
+						current = next;	
+					}
+				
+					else if ( frequency == "monthly" && ( current.getFullYear() == next.getFullYear() ) && ( (current.getMonth()+1) == (next.getMonth()+1) ) ) { // note: month starts with 0
+						current = next;	
+					}
+								
+					else if ( frequency == "annual" && ( current.getFullYear() == next.getFullYear() ) ) {
+						current = next;	
+					}
+
+					else {
+						for (var k = 0; k < numCols; k++) {
+							outputObj.data[k].push(inputObj.data[k][j-1]);
+						}
+						current = next;					
+					}
+				}
+			
+				// always push last row
+				for (var k = 0; k < numCols; k++) {
+					outputObj.data[k].push(inputObj.data[k][numRows-1]);
+				}
+		
+			}		
+			else
+				return null; // TODO: reverse chronological?
+		}
+		else
+			return null; // bad format
+			
+		return outputObj;
+	}
+
+	// TEST
+
+	// Options: weekly, monthly, annual
+	// var series = {"data":[["2014/03/27","2014/04/28","2014/04/30"],[13100,41100,10800],[558.46,559.99,556.97]]};
+
+	// var series = require('./test/goog_daily.json');
+	// console.log(convertSeriesFrequency(series, "monthly"));
+
+
+
+    // Exports and modularity
+	var a = {};
+	a.convert = convertSeriesFrequency;
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = a;
+    }
+
+    if (typeof ender === 'undefined') {
+        // this.convert = convertSeriesFrequency;
+        window.dm = a;
+    }
+
+    if (typeof define === "function" && define.amd) {
+        define('dm', [], function () { 
+            return a; 
+        });
+    }
+
+}).call(this);
