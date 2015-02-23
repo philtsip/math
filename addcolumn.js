92ca3610eed609f4f22e9f4db112939f67ce571d
Philipp Tsipman <philtsip@yahoo.com>
2015-02-22 22:07:34 -0500

    addcolumn: created logic - but not i/o for storage of repeat data

---
+++ addcolumn.js
@@ -0,0 +1,24 @@
+var metric = "adj_close";
+var save_flag = 1;
+var mcolumn = am.colforMetric(series, metric);
+var values = [];
+
+var comparisons_eng = ["monthly", "annual", "5yr", "7yr", "10yr"];
+var comparisons = [1,12,60,84,120];
+
+var dcolumn = am.colforMetric(series, metric + "_pdiff_" + comparisons[i]);
+
+if ( dcolumn !== null) {
+	values = series.data[dcolumn];	
+	save_col = 0;	
+}
+else {
+	values = am.percentdiffArray(series.data[mcolumn],comparisons[i]);	
+	save_col = 1;
+}
+	
+
+if (save_flag*save_col) {
+	series.meta.columns.push(metric + "_pdiff_" + comparisons[i]);
+	series.data.push(values);	
+}
